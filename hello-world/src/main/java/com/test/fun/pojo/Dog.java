82da605880e8fd0b8f99643cd2a2e4f4cb49958a
Jirka Pinkas <jirka.pinkas@gmail.com>
2020-10-27 14:08:06 +0100

    + module info (nyni se pouziva Lombok)!!!

--- hello-world/src/main/java/com/test/fun/pojo/Dog.java
+++ hello-world/src/main/java/com/test/fun/pojo/Dog.java
@@ -1,5 +1,12 @@
 package com.test.fun.pojo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Dog {
 
     private String name;
@@ -8,45 +15,4 @@ public class Dog {
 
     private int age;
 
-    public Dog() {
-    }
-
-    public Dog(String name, String race, int age) {
-        this.name = name;
-        this.race = race;
-        this.age = age;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getRace() {
-        return race;
-    }
-
-    public void setRace(String race) {
-        this.race = race;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    @Override
-    public String toString() {
-        return "Dog{" +
-                "name='" + name + '\'' +
-                ", race='" + race + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
---
+++ hello-world/src/main/java/module-info.java
@@ -0,0 +1,3 @@
+module hello.world {
+    requires lombok;
+}
